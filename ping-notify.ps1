3eec5ff67a77c694dac9194d2212fc6a01433e9b
Flibble <58303817+flibble-err@users.noreply.github.com>
2019-11-28 20:18:04 +0100

    Add files via upload

---
+++ ping-notify.ps1
@@ -0,0 +1,20 @@
+﻿If (
+Test-Connection LOCALHOST -Count 2 -Quiet -Delay 60
+) #Change the IP to whatever server you want to ping. Delay is the frequency of pings.
+
+{
+Write-Host "Connection successful at $(get-date)" -ForegroundColor Green
+    $player = New-Object -TypeName System.Media.SoundPlayer
+    $player.SoundLocation = "C:\Windows\Media\ring06.wav"
+    $player.Load()
+    $player.Play()
+    $wshell = New-Object -ComObject Wscript.Shell
+    $Output = $wshell.Popup("The server is now accessible!",0,"Server online",4160)
+   
+break
+} #When ping is successful
+else 
+{
+    "$(get-date) - Connection failed"
+    Invoke-Expression -Command ($PSCommandPath) #Run the script again
+} #When ping fails
